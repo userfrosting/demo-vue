f41b2aeac8093733984d8c515564f40eb63fbf75
Louis Charette <charette.louis@gmail.com>
2023-09-12 21:52:51 -0400

    Suppress/Fix warning

--- app/assets/views/HomeView.vue
+++ app/assets/views/HomeView.vue
@@ -23,13 +23,14 @@ getList()
     <div class="uk-container">
         <div style="padding-top: 50px;"></div>
         <article class="uk-article">
-            <h1 class="uk-article-title"><a class="uk-link-reset" href="">Hello!</a></h1>
+            <h1 class="uk-article-title"><a class="uk-link-reset" href="#">Hello!</a></h1>
             <p class="uk-article-meta">Written by <a href="#">Aravind Voggu</a> on 29 September 2018.</p>
             <p class="uk-text-lead">This is a demo of using UiKit with VueJS, built using Vue CLI 3.0</p>
             <p>The tutorial is available on my website - https://aravindvoggu.in. All the content below is just for example
                 and I've not added any functionality.</p>
 
-            <href src="https://aravindvoggu.in/uikit-with-vuejs-vue-cli-3-db811e43c46b">Link to the tutorial.</href>
+            <p><a href="https://aravindvoggu.in/uikit-with-vuejs-vue-cli-3-db811e43c46b">Link to the tutorial</a></p>
+            
             <div class="uk-grid-small uk-child-width-auto" uk-grid>
                 <div>
                     <a class="uk-button uk-button-text" href="#">Read more</a>
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const Encore = require('@symfony/webpack-encore');
+const webpack = require('webpack');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
@@ -21,6 +22,10 @@ Encore
     .enableSassLoader()
     .enableLessLoader()
     .enableBuildNotifications()
+    .addPlugin(new webpack.DefinePlugin({
+        __VUE_OPTIONS_API__: true,
+        __VUE_PROD_DEVTOOLS__: false,
+    }))
     // .configureDevServerOptions(options => {
     //     options.port = 8080;
     //     options.liveReload = true;
