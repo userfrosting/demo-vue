c7ca8a8399ef752fdd928e6915161868d6727ebc
Louis Charette <charette.louis@gmail.com>
2024-04-11 21:25:22 -0400

    Implement UserCard

--- app/assets/App.vue
+++ app/assets/App.vue
@@ -1,7 +1,8 @@
 <script setup>
 import { MainContent } from '@userfrosting/theme-pink-cupcake/Content'
-import { NavBar } from '@userfrosting/theme-pink-cupcake/NavBar'
-import { NavBarItem } from '@userfrosting/theme-pink-cupcake/NavBar'
+import { NavBar, NavBarItem, NavBarUserCard, NavBarUserCardButton } from '@userfrosting/theme-pink-cupcake/NavBar'
+import { useAuthStore } from './stores/auth.js'
+const auth = useAuthStore()
 </script>
 
 <template>
@@ -10,6 +11,9 @@ import { NavBarItem } from '@userfrosting/theme-pink-cupcake/NavBar'
         <NavBarItem to="/about" label="About" />
         <NavBarItem to="/resources" label="Api Test" />
         <NavBarItem to="/auth" label="Login Test" />
+        <NavBarUserCard v-if="auth.auth" :username="auth.user.full_name" :avatar="auth.user.avatar" :meta="auth.user.user_name">
+            <NavBarUserCardButton label="Logout" @click="auth.logout()" />
+        </NavBarUserCard>
     </NavBar>
     <MainContent>
         <RouterView />
